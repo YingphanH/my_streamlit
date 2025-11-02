7756b1efe496d5d858541bd425df7a406305b4f0
YingphanH <yingphan.h@gmail.com>
2025-11-02 11:16:56 +0700

    Update uber_pickups.py

--- uber_pickups.py
+++ uber_pickups.py
@@ -4,7 +4,8 @@ import numpy as np
 
 st.title('Starbucks visualize')
 
-DATE_COLUMN = 'date/time'DATA_URL = 'https://raw.githubusercontent.com/YingphanH/my_streamlit/main/directory.csv'
+DATE_COLUMN = 'date/time'
+DATA_URL = 'https://raw.githubusercontent.com/YingphanH/my_streamlit/main/directory.csv'
 
 @st.cache_data
 def load_data(nrows):
@@ -36,5 +37,6 @@ st.bar_chart(hist_values)
 #st.subheader(f'Map of all pickups at {hour_to_filter}:00')
 
 #st.map(filtered_data)
+
 
 
